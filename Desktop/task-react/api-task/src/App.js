122ef507a734168945876f65215528764851caaa
Cheeqito <bkinetix@gmail.com>
2022-04-24 21:14:27 +0100

    first commit

---
+++ Desktop/task-react/api-task/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UsersProfile from './UsersProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UsersProfile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/task-react/api-task/src/UsersProfile.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {Row, Col, Container} from 'react-bootstrap';
+
+const UsersProfile = () => {
+    const url = 'https://jsonplaceholder.typicode.com/users'
+    const [Users, setUsers] = useState(null)
+    const [Address, setAddress] = useState(null)
+
+    let Content = null;
+
+    useEffect(() => {
+        axios.get(url)
+        .then(response => {
+            setUsers(response.data)
+            setAddress(response.data)
+        })
+    
+    }, [url])
+
+    if(Users) {
+        Content =
+            <Container className='py-3'>
+                <h2 className='text-white text-center'>Users Profile</h2>
+                <Row>
+                    {Users.map((User) => (
+                        <Col sm={12} md={6} lg={4} className="pt-3 ">
+                        <div className='card p-3 mb-3 border-0' key={User.id}>
+                            <h2 className='card-title fw-bold'>{User.name}</h2>
+                            <p className='card-subtitle'>{User.username}</p>
+                            <div>
+                                <h6 className='fw-bold pt-4'>General Info</h6>
+                                <p className='card-content '>Email:<span className='ps-3'>{User.email}</span></p>
+                                
+                            </div>
+                        </div>
+                        </Col>
+                        ))}
+                </Row>
+            </Container>
+    }
+    
+    return(
+        <div>
+            {Content}
+        </div>
+    )
+}
+
+export default UsersProfile
