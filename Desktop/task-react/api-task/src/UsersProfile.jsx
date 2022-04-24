745adf23915b49d191c11f03f7d3350c9f545886
Cheeqito <bkinetix@gmail.com>
2022-04-24 22:39:51 +0100

    second commit

--- Desktop/task-react/api-task/src/UsersProfile.jsx
+++ Desktop/task-react/api-task/src/UsersProfile.jsx
@@ -5,7 +5,6 @@ import {Row, Col, Container} from 'react-bootstrap';
 const UsersProfile = () => {
     const url = 'https://jsonplaceholder.typicode.com/users'
     const [Users, setUsers] = useState(null)
-    const [Address, setAddress] = useState(null)
 
     let Content = null;
 
@@ -13,7 +12,6 @@ const UsersProfile = () => {
         axios.get(url)
         .then(response => {
             setUsers(response.data)
-            setAddress(response.data)
         })
     
     }, [url])
@@ -23,19 +21,24 @@ const UsersProfile = () => {
             <Container className='py-3'>
                 <h2 className='text-white text-center'>Users Profile</h2>
                 <Row>
-                    {Users.map((User) => (
-                        <Col sm={12} md={6} lg={4} className="pt-3 ">
-                        <div className='card p-3 mb-3 border-0' key={User.id}>
+                    {Users.map((User) => {
+                        const {street, suite, city, geo:{lat, lng}} = User.address
+                        return(
+                            <Col sm={12} md={6} lg={4} className="pt-3 ">
+                            <div className='card p-3 mb-3 border-0' key={User.id}>
                             <h2 className='card-title fw-bold'>{User.name}</h2>
                             <p className='card-subtitle'>{User.username}</p>
                             <div>
                                 <h6 className='fw-bold pt-4'>General Info</h6>
                                 <p className='card-content '>Email:<span className='ps-3'>{User.email}</span></p>
-                                
+                                <p className='card-text'>Address:<span className='ps-3'>{street} {suite} {city}</span></p>
+                                <p className='card-text'>geo:<span className='ps-3'>{lat} {lng} </span></p>
                             </div>
                         </div>
                         </Col>
-                        ))}
+                        )
+                    }
+                    )}
                 </Row>
             </Container>
     }
